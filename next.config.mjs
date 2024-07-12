d508d79bf79d1d58690e7e422895846cb7989221
AHMED MOHAMED ELTAYEB ELFAKI <159654325+TYB-hue@users.noreply.github.com>
2024-07-12 20:04:12 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,18 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  target: 'experimental-serverless-trace', // Replace with appropriate target option
 };
 
-export default withSentryConfig(nextConfig, {
-  // Sentry configuration options
-  org: "me-bdw",
-  project: "javascript-nextjs",
+const SentryWebpackPluginOptions = {
+  authToken: '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', // Replace with your actual Sentry auth token
+  org: 'me-bdw',
+  project: 'javascript-nextjs',
   silent: !process.env.CI,
   widenClientFileUpload: true,
   hideSourceMaps: true,
   disableLogger: true,
   automaticVercelMonitors: true,
-});
+};
+
+export default withSentryConfig(nextConfig, SentryWebpackPluginOptions);
