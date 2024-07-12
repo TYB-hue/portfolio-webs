cb2cc8ac6f46ecb42d70d90dc5ec74fa0f6d93d6
AHMED MOHAMED ELTAYEB ELFAKI <159654325+TYB-hue@users.noreply.github.com>
2024-07-13 02:57:02 +0300

    Update and rename next.config.js to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+import { withSentryConfig } from '@sentry/nextjs';
+
+const nextConfig = {
+  // Your Next.js configuration options
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+const SentryWebpackPluginOptions = {
+  authToken: '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', // Replace with your actual Sentry auth token
+  org: 'me-bdw',
+  project: 'javascript-nextjs',
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+};
+
+export default withSentryConfig(nextConfig, SentryWebpackPluginOptions);
