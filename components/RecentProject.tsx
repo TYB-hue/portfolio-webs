2a80e172fd8d9d1312938948be35744a4fe244fd
AHMED MOHAMED ELTAYEB ELFAKI <159654325+TYB-hue@users.noreply.github.com>
2024-08-26 19:46:38 +0300

    Update RecentProject.tsx
    delete icons
    
    Signed-off-by: AHMED MOHAMED ELTAYEB ELFAKI <159654325+TYB-hue@users.noreply.github.com>

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -11,7 +11,7 @@ const RecentProject = () => {
       <span className='text-purple'>RecentProject</span>
         </h1>
         <div className='flex flex-wrap items-center justify-center p-4 gap-y-20 mt-10'>
-            {projects.map(({id,title,des,img,iconLists,link}) => (
+            {projects.map(({id,title,des,img,link}) => (
                 <div key={id} className='sm:h-[41rem] h-[25rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw] '>
                     <PinContainer title={link} href={link}>
                         <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
@@ -28,14 +28,7 @@ const RecentProject = () => {
                     <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>{des}</p>
                     
                     <div className='flex items-center justify-between mt-7 mb-3'>
-                        <div className='flex items-center'>
-                            {iconLists.map((icon, index)=>(
-                                <div key={icon} className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center'
-                                style={{transform:`translateX(-${5 * index * 2}px)`}}>
-                                    <img src={icon} alt={icon} className='p-2' />
-                                </div>
-                            ))}
-                        </div>
+                        
                         <div className='flex justify-center items-center'>
                             
                             <p className='flex lg:text-xl md:text-xs text-sm text-purple'>Check Live Site</p>
